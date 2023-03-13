1e9f8e0d17e156bec52226ab77dcad9db3a3b42e
Jonz De Paz <82692360+jonzdepaz018@users.noreply.github.com>
2023-03-13 21:56:02 +0800

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
-
+import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
-  private _http: Http
 
-  constructor() { }
 
+private _albumUrl = '../assets/album.json';
+  constructor(  private _http: Http) { }
+
+  getAlbum(id: number) {
+     return this._http.get(this._albumUrl)
+  }
 }
